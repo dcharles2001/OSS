4c7ef379bd077428445d29a4e27c216f0db545db
dcharles2001 <daniel.charles@students.plymouth.ac.uk>
2023-11-21 12:31:55 +0000

    Update Node_2.ino

--- Node 2(RasPiPico)/Sensor_Testing/Node_2/Node_2.ino
+++ Node 2(RasPiPico)/Sensor_Testing/Node_2/Node_2.ino
@@ -1,12 +1,12 @@
 
 // Include NewPing Library
-#include "NewPing.h"
+#include <NewPingESP8266.h>
 
 // Hook up HC-SR04 with Trig to Arduino Pin 9, Echo to Arduino pin 10
-#define TRIGGER_PIN 9
-#define ECHO_PIN 10
+#define TRIGGER_PIN 12 //D6
+#define ECHO_PIN 14  //D5
 
-#define sensorPin 7
+#define sensorPin 13 //D7
 
 // Maximum distance we want to ping for (in centimeters).
 #define MAX_DISTANCE 400
@@ -17,7 +17,7 @@ int Sound;
 int Alarm = 0;
 
 // NewPing setup of pins and maximum distance.
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+NewPingESP8266 sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 void setup() {
   pinMode(sensorPin, INPUT);
@@ -26,7 +26,7 @@ void setup() {
 
 void loop() {
   Dist = ((sonar.ping() / 2) * 0.0343);
-  sensorValue = digitalRead(sensorPin);
+  int sensorValue = digitalRead(sensorPin);
   if (Dist > OPEN_DISTANCE){
     Alarm = 1;
     Serial.println("Safe Open");
